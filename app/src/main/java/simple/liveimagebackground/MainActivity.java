bc87a9c68278fd533a02f9f3b68aaafafbc1fe68
Robert Strzępka <robert.strzepka@appunite.com>
2016-04-18 13:46:03 +0200

    Little naming fix

--- app/src/main/java/simple/liveimagebackground/MainActivity.java
+++ app/src/main/java/simple/liveimagebackground/MainActivity.java
@@ -8,7 +8,7 @@ import simple.liveimagelibrary.LiveBackgroundImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    LiveBackgroundImageView mImage;
+    LiveBackgroundImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +19,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onContentChanged() {
         super.onContentChanged();
-        mImage = (LiveBackgroundImageView) findViewById(R.id.image);
+        imageView = (LiveBackgroundImageView) findViewById(R.id.image);
     }
 
     @Override
     protected void onDestroy() {
-        mImage.stopGyroscope();
+        imageView.stopGyroscope();
         super.onDestroy();
     }
 }
